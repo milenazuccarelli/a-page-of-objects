d5b234b620c3d9229546e35b510f08ef2b06f617
piccolazucca <milena.zuccarelli@gmail.com>
2017-03-31 16:03:32 +0100

    modified pages, added media queries

--- scroll.js
+++ scroll.js
@@ -1,35 +1,35 @@
-if( $window.width() > 800){
 
-$.fn.moveIt = function(){
-  var $window = $(window);
-  var instances = [];
-  
-  $(this).each(function(){
-    instances.push(new moveItItem($(this)));
-  });
-  
-  window.onscroll = function(){
-    var scrollTop = $window.scrollTop();
-    instances.forEach(function(inst){
-      inst.update(scrollTop);
+  if ($(window).width() > 660) {
+    $.fn.moveIt = function(){
+      var $window = $(window);
+      var instances = [];
+
+      $(this).each(function(){
+        instances.push(new moveItItem($(this)));
+      });
+
+      window.onscroll = function(){
+        var scrollTop = $window.scrollTop();
+        instances.forEach(function(inst){
+          inst.update(scrollTop);
+        });
+      }
+    }
+
+    var moveItItem = function(el){
+      this.el = $(el);
+      this.speed = parseInt(this.el.attr('data-scroll-speed'));
+    };
+
+    moveItItem.prototype.update = function(scrollTop){
+      this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
+    };
+
+    // Initialization
+    $(function(){
+      $('[data-scroll-speed]').moveIt();
     });
-  }
 }
-
-var moveItItem = function(el){
-  this.el = $(el);
-  this.speed = parseInt(this.el.attr('data-scroll-speed'));
-};
-
-moveItItem.prototype.update = function(scrollTop){
-  this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
-};
-
-// Initialization
-$(function(){
-  $('[data-scroll-speed]').moveIt();
-});
-
 
 // rotate the fruit label
 $(window).scroll(function() {
@@ -37,4 +37,3 @@ var theta = $(window).scrollTop() / 20 % Math.PI;
 $('.spin').css({ transform: 'rotate(' + theta + 'rad)' });
 $('.spin-reverse').css({ transform: 'rotate(-' + theta + 'rad)' });
 });
-}
