b5f2bf5a93b3b5ca39040ebc65e726cb5786da72
piccolazucca <milena.zuccarelli@gmail.com>
2017-03-31 20:00:10 +0100

    Did some polishing

--- scroll.js
+++ scroll.js
@@ -1,6 +1,4 @@
-
-  if ($(window).width() > 660) {
-    $.fn.moveIt = function(){
+$.fn.moveIt = function(){
       var $window = $(window);
       var instances = [];
 
@@ -25,11 +23,32 @@
       this.el.css('transform', 'translateY(' + -(scrollTop / this.speed) + 'px)');
     };
 
-    // Initialization
     $(function(){
       $('[data-scroll-speed]').moveIt();
     });
+
+function windowSize() {
+  windowHeight = window.innerHeight ? window.innerHeight : $(window).height();
+  windowWidth = window.innerWidth ? window.innerWidth : $(window).width();
+
 }
+
+//Init Function of init it wherever you like...
+windowSize();
+
+// For example, get window size on window resize
+$(window).resize(function() {
+  windowSize();
+  console.log('width is :', windowWidth, 'Height is :', windowHeight);
+  if (windowWidth < 660) {
+        // Initialization
+    $(function(){
+      $('[data-scroll-speed]').moveIt().event.stopPropagation();
+    });
+
+  }
+});
+
 
 // rotate the fruit label
 $(window).scroll(function() {
